ddb40f2a8e0aea7ae5bf80fc1ac2b22e29e5fb1a
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2025-11-04 11:03:57 +0700

    fix(services): remove VisitorService export from index

--- src/lib/services/index.ts
+++ src/lib/services/index.ts
@@ -10,5 +10,4 @@ export { JobService } from './job.service';
 export { CompanyService } from './company.service';
 export { AdminService } from './admin.service';
 export { AdminAuthService } from './admin-auth.service';
-export { VisitorService } from './visitor.service';
 export type { Report, Company, ReportStatus, ReportType } from './admin.service';
